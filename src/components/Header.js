63205f3a9859f8e7eb66f895325c0e8ec413d76a
Bouhzial <bouhzilaahmed2@gmail.com>
2021-12-17 19:06:21 +0100

    hello

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 
 const Header = () => {
     return (
-        <div className="title"><h1>le calcule des possibilités</h1>
+        <div className="title"><h2>le calcule des possibilités</h2>
              <p>Etant donné un ensemble E de n objets,<br />
              donner n le nombre des element et p objets pris parmi les n objects <br/>
              et remplir les cases à cocher c'est la remis est permis ou l'ordre est important   </p>
--- src/components/test.js
+++ src/components/test.js
@@ -6,9 +6,13 @@ class SimpleForm extends Component {
     this.state = {
       n:'',
       p:'',
+      k:'',
       order:false,
       remis:false,
-      resultata:0
+      resultata:'',
+      type:'',
+      avec:'',
+      isK: false,
     };
     this.onInputchange = this.onInputchange.bind(this);
     this.onSubmitForm = this.onSubmitForm.bind(this);
@@ -26,32 +30,47 @@ class SimpleForm extends Component {
     });
   }
 
-  factorial=(x) =>{
-    if (x === 0) {
-        return 1;
+  factorial=(num) =>{
+    var result = num;
+    if (num === 0 || num === 1) 
+      return 1; 
+    while (num > 1) { 
+      num--;
+      result *= num;
     }
-    else {
-        return x * this.factorial(x - 1);
-    }
-}
+    return result;
+  }
+
+  AddK =()=>{
+
+  }
+
   onSubmitForm() {
     console.log(this.state)
     const y =this.state.n-this.state.p;
     const y1 = (Number(this.state.n)+Number(this.state.p))-1;
     console.log(y1);
     var resultat;
-    if(this.state.order===false && this.state.remis===true && y>0 ){
-      resultat=(Math.pow(this.state.n,this.state.p))}
-    else if(this.state.order===false && this.state.remis===false && y>0 ){
-      resultat=(this.factorial(this.state.n)/this.factorial(y))}
-    else if(this.state.order===false && this.state.remis===false && y===0 ){
-      resultat=(this.factorial(this.state.n))}
+    var remi;
+    var typ;
+    if(this.state.order===true && this.state.remis===true && y>0 ){
+      resultat=(Math.pow(this.state.n,this.state.p))
+      typ='Arrangement'; remi ="avec remis "}
     else if(this.state.order===true && this.state.remis===false && y>0 ){
-       resultat=(this.factorial(this.state.n)/(this.factorial(this.state.p)*this.factorial(y))) }
-    else if(this.state.order===true && this.state.remis===true && y>0 ){
-       resultat=this.factorial(y1)/(this.factorial(this.state.p)*this.factorial((this.state.n)-1))}
-      else{resultat='pas possilble '}
-       this.setState({resultata:resultat})
+      resultat=(this.factorial(this.state.n)/this.factorial(y)) ;typ='Arrangement' ;remi ="sans remis "}
+    else if(this.state.order===true && this.state.remis===false && y===0 ){
+      resultat=(this.factorial(this.state.n)) ;typ='Permutation' ;remi ="sans remis "}
+      else if(this.state.order===true && this.state.remis===true && y===0){
+           this.setState({isK:!this.state.isK});
+           if(this.state.k>this.state.n){resultat=" k> n c'est imposible"}else{
+           resultat=(this.factorial(this.state.n)/this.factorial(this.state.k)) ;typ='Permutation' ;remi ="avec remis "
+      }this.setState({k:''})}
+    else if(this.state.order===false && this.state.remis===false && y>0 ){
+       resultat=(this.factorial(this.state.n)/(this.factorial(this.state.p)*this.factorial(y))) ;typ='Combinaison' ;remi ="sans remis "}
+    else if(this.state.order===false && this.state.remis===true && y>0 ){
+       resultat=this.factorial(y1)/(this.factorial(this.state.p)*this.factorial((this.state.n)-1));typ='Combinaison' ;remi ="avec remis "}
+      else{resultat='imposible de calcule' ; typ = 'error'}
+       this.setState({resultata:resultat ,type:typ ,avec:remi})
     }
    render() {
     return (
@@ -80,6 +99,18 @@ class SimpleForm extends Component {
             />
           </label>
         </div>
+        <div>{this.state.isK? 
+        <label>Dans le cas où il existerait plusieurs répétitions k d’un même objet parmi les n objets
+            <input
+              className="cb"
+              name="k"
+              placeholder="la valeur de k"
+              type="number"
+              value={this.state.k}
+              onChange={this.onInputchange}
+            />
+          </label> : null}
+        </div>
         <div>
         <input className="cb" name='order'value={this.state.order} defaultChecked={this.state.ordre} type='checkbox' onClick={this.toggleChange}/> <span>l'ordre est impotant?</span>
         </div><div>
@@ -87,6 +118,9 @@ class SimpleForm extends Component {
         </div>
         <div>
             <button className="cb" onClick={this.onSubmitForm}>calcuer</button>
+        </div>
+        <div>
+          <p>le type de calcule est : {this.state.type} {this.state.avec} </p>
         </div>
         <div>
             <p>le nombre de possibilité:  {this.state.resultata}</p>
